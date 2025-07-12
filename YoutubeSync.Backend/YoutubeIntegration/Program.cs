2837e6c1ac6a5b223333e0921cf2391bf6615ceb
Alec Sabbath <alec.sabbath@gmail.com>
2025-07-12 08:42:20 -0500

    Adding basic start to retreiving google apis

--- YoutubeSync.Backend/YoutubeIntegration/Program.cs
+++ YoutubeSync.Backend/YoutubeIntegration/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace YoutubeIntegration
+﻿using Google.Apis.Services;
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+
+namespace YoutubeIntegration
 {
     public class Program
     {
-        public async Task Start()
+        [STAThread]
+        static void Main(string[] args)
         {
-            var service = new 
+            Console.WriteLine("Discovery API Sample");
+            Console.WriteLine("====================");
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            // Create the service.
+            var service = new DiscoveryService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Discovery Sample",
+                ApiKey = "I am not writing my API key here",
+            });
+
+            // Run the request.
+            Console.WriteLine("Executing a list request...");
+            var result = await service.Apis.List().ExecuteAsync();
+
+            // Display the results.
+            if (result.Items != null)
+            {
+                foreach (DirectoryList.ItemsData api in result.Items)
+                {
+                    Console.WriteLine(api.Id + " - " + api.Title);
+                }
+            }
         }
     }
 
