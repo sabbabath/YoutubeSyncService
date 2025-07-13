8e9146e2d52aee99040fc6159baa71f5de5d33d9
Alec Sabbath <alec.sabbath@gmail.com>
2025-07-13 10:18:56 -0500

    Adding spotify urls start

---
+++ YoutubeSync.Backend/SpotifyIntegration/Models/cSpotifyUrls.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SpotifyIntegration.Models
+{
+    public class cSpotifyUrls
+    {
+        public static readonly Uri AuthorizeUri = new("https://accounts.spotify.com/authorize");
+
+        public static readonly Uri V1Uri = new("https://api.spotify.com/v1/");
+
+        public static readonly Uri OAuthTokenUri = new("https://accounts.spotify.com/api/token");
+
+        public static Uri TopTracks() => new("me/top/tracks");
+
+        public static Uri TopArtists() => new("me/top/artists");
+
+        public static Uri User(string userId) => new($"users/{userId}");
+
+        public static Uri Search() => new("search");
+    }
+}
