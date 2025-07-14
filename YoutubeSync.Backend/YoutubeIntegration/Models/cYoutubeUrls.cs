5c0a159f14bb5b6e811ab4c72a7bcd1e2ed0a103
Alec Sabbath <alec.sabbath@gmail.com>
2025-07-14 11:20:50 -0500

    Adding youtube apis

---
+++ YoutubeSync.Backend/YoutubeIntegration/Models/cYoutubeUrls.cs
@@ -0,0 +1,19 @@
+﻿namespace YoutubeIntegration.Models
+{
+    public class cYoutubeUrls
+    {
+        public static readonly Uri AuthorizeUri = new("");
+
+        public static readonly Uri V3Uri = new("https://www.googleapis.com/youtube/v3");
+
+        public static readonly Uri OAuthTokenUri = new("");
+
+        public static Uri Search() => new("search");
+
+        public static Uri Videos() => new("videos");
+
+        public static Uri Playlists() => new("playlists");
+
+
+    }
+}
